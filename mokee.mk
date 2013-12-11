ce62c158134d83e6d5c5817b4552e9ac99574c9d
martincz <martincz@mokeedev.com>
2013-12-11 09:10:30 +0800

    su640: mokee modify
    Change-Id: I1dfe1b5fcf49d060dd538e921d596ce872320e5e

--- mokee.mk
+++ mokee.mk
@@ -1,20 +1,20 @@
 ## Specify phone tech before including full_phone
-$(call inherit-product, vendor/cm/config/gsm.mk)
+$(call inherit-product, vendor/mk/config/gsm.mk)
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1280
 TARGET_SCREEN_WIDTH := 720
 
-# Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+# Inherit some common MK stuff.
+$(call inherit-product, vendor/mk/config/common_full_phone.mk)
 
 # This device has NFC
-$(call inherit-product, vendor/cm/config/nfc_enhanced.mk)
+$(call inherit-product, vendor/mk/config/nfc_enhanced.mk)
 
 # Inherit device configuration
 $(call inherit-product, device/lge/su640/su640.mk)
 
-PRODUCT_NAME := cm_su640
+PRODUCT_NAME := mk_su640
 
 # Release name and versioning
 PRODUCT_RELEASE_NAME := OptimusLTE
